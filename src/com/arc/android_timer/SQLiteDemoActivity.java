c220eea27e22cdff3210033b1dd004a4e77c2c2b
ArcSung <ArcSung@via.com.tw>
2014-05-20 14:48:53 +0800

    add Update & remove feature for SQL Data Base

--- src/com/arc/android_timer/SQLiteDemoActivity.java
+++ src/com/arc/android_timer/SQLiteDemoActivity.java
@@ -8,22 +8,33 @@ import static com.arc.android_timer.DbConstants.TABLE_NAME;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import at.abraxas.amarino.Amarino;
 
 public class SQLiteDemoActivity extends Activity {
@@ -62,13 +73,13 @@ public class SQLiteDemoActivity extends Activity {
 
 	private void initView(){
 		listData = (ListView) findViewById(R.id.listData);
-		listData.setOnItemLongClickListener(new OnItemLongClickListener(){
+    	listData.setOnItemLongClickListener(new OnItemLongClickListener(){
 
 			@Override
 			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
 					int pos, long id) {
 				// TODO Auto-generated method stub
-				Log.i("android_timer", "pos: "+pos);
+				SQLdb_Dialog(id);
 				return false;
 			}
 			
@@ -96,36 +107,114 @@ public class SQLiteDemoActivity extends Activity {
     	
     	return cursor;
     }
+    
+    private Cursor fetehData(long rowID) throws SQLException
+    {   	
+    	SQLiteDatabase db = dbhelper.getReadableDatabase();
+    	String[] columns = {_ID, Date, Time, Remark};
+    	
+    	Cursor cursor = db.query(true, TABLE_NAME, columns, _ID + "=" + rowID, null, null, null, null, null);
+        
+    	if(cursor !=null)
+    		cursor.moveToFirst();
+    	  	
+    	return cursor;
+    }
         
     
     private void showInList(){
     	
     	Cursor cursor = getCursor();
     	
-    	String[] from = {Date, Time, Remark};
-    	int[] to = {R.id.txtDate, R.id.txtTime, R.id.txtRemark};
+    	String[] from = {_ID, Date, Time, Remark};
+    	int[] to = {R.id.txtID, R.id.txtDate, R.id.txtTime, R.id.txtRemark};
     	
     	SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.data_item, cursor, from, to);
     	listData.setAdapter(adapter);
     }
 
-    private void del(){
-    	//String id = editId.getText().toString();
+    private void del(long rowID){
     	
-    	//SQLiteDatabase db = dbhelper.getWritableDatabase();
-    	//db.delete(TABLE_NAME, _ID + "=" + id, null);
+    	Log.i("android_timer", "id: "+rowID);
+    	SQLiteDatabase db = dbhelper.getWritableDatabase();
+    	db.delete(TABLE_NAME, _ID + "=" + rowID, null);
     }
 	
-    private void update(){
-    	//String id = editId.getText().toString();
+    private void update(long rowID, String Date_db,  String Time_db, String Remark_db){
     	
-    	//ContentValues values = new ContentValues();
-    	//values.put(Date, editName.getText().toString());
-    	//values.put(Time, editTel.getText().toString());
-    	//values.put(Remark, editEmail.getText().toString());
+    	ContentValues values = new ContentValues();
+    	values.put(Date,  Date_db);
+    	values.put(Time, Time_db);
+    	values.put(Remark, Remark_db);
     	
-    	//SQLiteDatabase db = dbhelper.getWritableDatabase();
-    	//db.update(TABLE_NAME, values, _ID + "=" + id, null);
+    	Log.i("android_timer", "id: "+rowID);
+    	Log.i("android_timer", "Date_db: "+Date_db);
+    	Log.i("android_timer", "Time_db: "+Time_db);
+    	Log.i("android_timer", "Remark_db: "+Remark_db);
+    	SQLiteDatabase db = dbhelper.getWritableDatabase();
+    	db.update(TABLE_NAME, values, _ID + "=" + rowID, null);
     }
+    
+	private void SQLdb_Dialog(final long RowID)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Detail");
+		Log.i("android_timer", "id_dialog: "+RowID);
+		//init SQL
+	    Cursor cursor = fetehData(RowID);
+    	    	
+    	final String Date_db   = cursor.getString(1);
+    	final String Time_db   = cursor.getString(2);
+    	String Remark_db = cursor.getString(3);
+		
+		// Setting_Dialog UI set up
+		LinearLayout linear=new LinearLayout(this);
+		linear.setOrientation(1);
+				
+		TextView DateText = new TextView(this);
+		DateText.setTypeface(null, Typeface.BOLD);
+		DateText.setText("Date: "+Date_db);
+				
+		TextView TimeText = new TextView(this);
+		TimeText.setTypeface(null, Typeface.BOLD);
+		TimeText.setText("Time :"+Time_db);
+			
+		TextView RemarkText = new TextView(this);
+		RemarkText.setTypeface(null, Typeface.BOLD);
+		RemarkText.setText("Remark: ");	
+		
+		final EditText RemarkEdit = new EditText(this);
+		RemarkEdit.setTypeface(null, Typeface.BOLD);
+		RemarkEdit.setText(Remark_db);	
+		
+	    linear.addView(DateText);
+	    linear.addView(TimeText);
+	    linear.addView(RemarkText);
+	    linear.addView(RemarkEdit);
+
+	    builder.setView(linear); 
+				
+		
+		builder.setPositiveButton("Update", new DialogInterface.OnClickListener() 
+		{
+	        public void onClick(DialogInterface dialog, int id) 
+	        {
+	        	update(RowID, Date_db, Time_db, RemarkEdit.getText().toString());
+	        	showInList();
+	        }
+	    });
+		
+	    builder.setNegativeButton("Delet", new DialogInterface.OnClickListener() 
+	    {
+	        public void onClick(DialogInterface dialog, int id) 
+	        {
+	        	del(RowID);
+	        	showInList();
+	        }
+	    });
+	    
+	    AlertDialog setting_dialog = builder.create();
+	    setting_dialog.show();
+	}
     
 }
